2de8a2dd36fd68e3feae57b18f9bf01e25b67ad1
Денис Скурат <den.skur2016@yandex.ru>
2021-11-17 23:23:50 +0300

    qualification was added

---
+++ KursachMikhalkevich/Controllers/QualificationController.cs
@@ -0,0 +1,144 @@
+﻿using KursachMikhalkevich.Data;
+using KursachMikhalkevich.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KursachMikhalkevich.Controllers
+{
+    public class QualificationController : Controller
+    {
+        private ApplicationDbContext _context;
+        public QualificationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ViewResult List()
+        {
+            var positions = _context.Qualifications.Include(t => t.Workers).Select(t => t);
+            return View(positions);
+        }
+
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            Qualification qualification = new Qualification();
+            return View(qualification);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Qualification qualification)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(qualification);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("List");
+            }
+            return View(qualification);
+        }
+
+        public IActionResult CheckName(int? Id, string? Name)
+        {
+
+            if (Id == null && Name != null)
+            {
+                var res1 = _context.Qualifications.Where(t => t.Name == Name).FirstOrDefault();
+                if (res1 == null)
+                {
+                    return Json(true);
+                }
+                return Json(false);
+            }
+            else if (Name != null)
+            {
+                var res1 = _context.Qualifications.Where(t => t.Id == Id).FirstOrDefault();
+                var res2 = _context.Qualifications.Where(t => t.Name == Name).FirstOrDefault();
+                if (res2 == null || res1.Id == res2.Id)
+                {
+                    return Json(true);
+                }
+                return Json(false);
+            }
+            return Json(false);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("List");
+            }
+            Qualification qualification = _context.Qualifications.Include(t => t.Workers).Where(t => t.Id == id).Select(t => t).FirstOrDefault();
+            if (qualification != null)
+            {
+                return View(qualification);
+            }
+            return RedirectToAction("List");
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Qualification qualification)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Entry(qualification).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                if (AuthorizedUser.GetUser()?.QualificationId == qualification.Id)
+                {
+                    var worker = AuthorizedUser.GetUser();
+                    worker.Qualification = qualification;
+                    AuthorizedUser.SetUser(worker);
+                }
+                return RedirectToAction("List");
+            }
+            qualification.Workers = _context.Workers.Where(t => t.QualificationId == qualification.Id).ToList();
+            return View(qualification);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            Qualification qualification = _context.Qualifications.Find(id);
+            if (qualification == null)
+            {
+                return RedirectToAction("List");
+            }
+            else if (qualification?.Id == AuthorizedUser.GetUser()?.QualificationId)
+            {
+                return RedirectToAction("List");
+            }
+
+            return View(qualification);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            Qualification qualification = _context.Qualifications.Find(id);
+            if (qualification == null)
+            {
+                return RedirectToAction("List");
+            }
+            else if (qualification?.Id == AuthorizedUser.GetUser()?.QualificationId)
+            {
+                return RedirectToAction("List");
+            }
+            _context.Qualifications.Remove(qualification);
+            _context.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
